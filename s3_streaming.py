15c3d5d09f7ce2d32f18a7b9db0eb59e60c71a83
oaoni <oa.oni7@gmail.com>
2018-06-08 16:11:58 -0400

    script for get/push operations to s3

---
+++ s3_streaming.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from io import StringIO
+from io import BytesIO
+import boto3
+
+def numpy_to_s3(client,data,bucket,key):
+    #Create a csv stream
+    csv_buffer = StringIO()
+    #Save numpy array as a csv to stream
+    np.savetxt(csv_buffer, data, delimiter=',')
+    #Convert the str to bytes
+    body = bytes(csv_buffer.getvalue())
+    #Save body to s3 in cooresponding bucket and key
+    client.put_object(Body=body, Bucket=bucket, Key=key)
+    
+def s3_to_numpy(client,bucket,key):
+    #Acquire object from s3
+    obj = client.get_object(Bucket=bucket, Key=key)
+    #Store read data from object in a bytes stream
+    bytes_stream = BytesIO(obj['Body'].read())
+    #Load data into a numpy array
+    np_array = np.loadtxt(bytes_stream, delimiter=',')
+    
+    return np_array
